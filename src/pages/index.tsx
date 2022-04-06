088d6254cb946346f5fb99ad127bc15b988a743e
Hirwa Blessing <hirwablessing7@gmail.com>
2022-04-06 15:38:59 +0200

    feat: fix the problem of nav

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,29 @@
 import Header from '@/components/layout/header'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { Fragment } from 'react'
+import { Fragment, useEffect } from 'react'
 
 const Home: NextPage = () => {
+    useEffect(() => {
+        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+        if (
+            localStorage.theme === 'dark' ||
+            (!('theme' in localStorage) &&
+                window.matchMedia('(prefers-color-scheme: dark)').matches)
+        ) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    }, [])
+
     return (
         <Fragment>
             <Head>
                 <title>Moogli</title>
             </Head>
 
-            <section>
+            <section className="dark">
                 <Header />
             </section>
         </Fragment>
