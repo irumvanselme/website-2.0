d23ca883e32869f539d588dd07ec35244da8ee57
Hirwa Blessing <hirwablessing7@gmail.com>
2022-04-07 17:26:48 +0200

    do somethings on thehero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,18 +1,23 @@
-import {useEffect} from "react";
+import { useEffect } from 'react'
 
-const CardContainer = ({number}: { number: number }) => {
+const CardContainer = ({ number }: { number: number }) => {
     return (
-        <div className='m-3 bg-white text-black text-[10px] flex justify-center items-center h-3.5'>{number}</div>
+        <div className="m-3 bg-white text-black text-[10px] flex justify-center items-center h-3.5">
+            {number}
+        </div>
     )
 }
 
 export default function Hero() {
     useEffect(() => {
         let division = document.getElementById('__division') as HTMLDivElement
-        if (!division) return;
+        if (!division) return
 
-        document.addEventListener("mousemove", (e) => {
-            let [x, y] = [2350 / 2 - (e.clientY * 2350 / window.innerHeight), 2820 / 2 - (e.clientX * 2820 / window.innerWidth)];
+        document.addEventListener('mousemove', e => {
+            let [x, y] = [
+                2350 / 2 - (e.clientY * 2350) / window.innerHeight,
+                2820 / 2 - (e.clientX * 2820) / window.innerWidth
+            ]
             // console.clear();
             division.style.transform = `translate(${x / 10}px, ${y / 10}px)`
         })
@@ -21,41 +26,41 @@ export default function Hero() {
     return (
         <>
             <div
-                className='h-screen bg-[#F7F9FC] dark:bg-black overflow-hidden flex items-center justify-center -mt-10 lg:-mt-16'
+                className="h-screen bg-[#F7F9FC] dark:bg-black overflow-hidden flex items-center justify-center -mt-10 lg:-mt-16"
                 id="__hero"
             >
-                <div className='h-[235px] w-[280px] scale-[10] bg-red-500 p-[20px] will-change-transform'>
-                    <div id='__division'>
-                        <div className='grid grid-cols-5'>
-                            <CardContainer number={1}/>
-                            <CardContainer number={2}/>
-                            <CardContainer number={3}/>
-                            <CardContainer number={4}/>
-                            <CardContainer number={5}/>
+                <div className="h-[235px] w-[280px] scale-[10] bg-red-500 p-[20px] will-change-transform">
+                    <div id="__division">
+                        <div className="grid grid-cols-5">
+                            <CardContainer number={1} />
+                            <CardContainer number={2} />
+                            <CardContainer number={3} />
+                            <CardContainer number={4} />
+                            <CardContainer number={5} />
                         </div>
-                        <div className='grid grid-cols-4'>
-                            <CardContainer number={6}/>
-                            <CardContainer number={7}/>
-                            <CardContainer number={8}/>
-                            <CardContainer number={9}/>
+                        <div className="grid grid-cols-4">
+                            <CardContainer number={6} />
+                            <CardContainer number={7} />
+                            <CardContainer number={8} />
+                            <CardContainer number={9} />
                         </div>
-                        <div className='grid grid-cols-4'>
-                            <CardContainer number={10}/>
-                            <CardContainer number={12}/>
-                            <CardContainer number={13}/>
-                            <CardContainer number={14}/>
+                        <div className="grid grid-cols-4">
+                            <CardContainer number={10} />
+                            <CardContainer number={12} />
+                            <CardContainer number={13} />
+                            <CardContainer number={14} />
                         </div>
-                        <div className='grid grid-cols-4'>
-                            <CardContainer number={15}/>
-                            <CardContainer number={16}/>
-                            <CardContainer number={17}/>
-                            <CardContainer number={18}/>
+                        <div className="grid grid-cols-4">
+                            <CardContainer number={15} />
+                            <CardContainer number={16} />
+                            <CardContainer number={17} />
+                            <CardContainer number={18} />
                         </div>
-                        <div className='grid grid-cols-4'>
-                            <CardContainer number={19}/>
-                            <CardContainer number={20}/>
-                            <CardContainer number={21}/>
-                            <CardContainer number={22}/>
+                        <div className="grid grid-cols-4">
+                            <CardContainer number={19} />
+                            <CardContainer number={20} />
+                            <CardContainer number={21} />
+                            <CardContainer number={22} />
                         </div>
                     </div>
                 </div>
